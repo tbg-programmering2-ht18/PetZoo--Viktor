33393d466a59d65416bb2931e936a82051b8e0f3
Viktor Fjällström <Viktor.DTK.Fjallstrom@lystkom.se>
2018-11-30 13:23:50 +0100

    Add project files.

---
+++ PetZoo/PET.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PetZoo
+{
+    class PET
+    {
+        
+        public string name  {get; set;}
+        public int id {get; set;}
+        public int age { get; set; }
+        
+    }
+}
